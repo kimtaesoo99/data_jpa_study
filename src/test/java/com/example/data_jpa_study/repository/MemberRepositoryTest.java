0d00a1a67a124a446ed98130779bc28f31b79d9a
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-11 13:59:34 +0900

    test: (MemberRepositoryTest) spring data jpa 레포 테스트 추가

---
+++ src/test/java/com/example/data_jpa_study/repository/MemberRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.example.data_jpa_study.repository;
+
+import com.example.data_jpa_study.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember(){
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
