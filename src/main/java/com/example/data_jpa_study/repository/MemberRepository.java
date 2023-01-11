d7beee61073f9ac0dde28da8ccf842737839ced2
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-11 19:48:00 +0900

    feat: (MemberRepository) 네임드 쿼리 이용해서 유저 찾기

--- src/main/java/com/example/data_jpa_study/repository/MemberRepository.java
+++ src/main/java/com/example/data_jpa_study/repository/MemberRepository.java
@@ -2,10 +2,14 @@ package com.example.data_jpa_study.repository;
 
 import com.example.data_jpa_study.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username,int age);
+
+    //@Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
 }
