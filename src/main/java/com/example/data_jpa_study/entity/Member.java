7cbe2af0bddeaf53903a5c501c34308c07939635
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-11 13:57:50 +0900

    feat: (Member) 멤버 엔티티 추가

---
+++ src/main/java/com/example/data_jpa_study/entity/Member.java
@@ -0,0 +1,26 @@
+package com.example.data_jpa_study.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    protected Member(){}
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+}
