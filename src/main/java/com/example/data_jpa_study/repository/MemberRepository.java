9b747d6c538ca469548b9186367771812a5ffa03
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-11 20:35:14 +0900

    feat: (MemberRepository) 반환 타입별 조회

--- src/main/java/com/example/data_jpa_study/repository/MemberRepository.java
+++ src/main/java/com/example/data_jpa_study/repository/MemberRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
@@ -26,4 +27,9 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
 
     @Query("select m from Member m where m.username in :names")
     List<Member> findByNames(@Param("names") List<String> names);
+
+
+    List<Member> findListByUsername(String username); //컬렉션
+    Member findMemberByUsername(String username); //단건
+    Optional<Member> findOptionalByUsername(String username); // 단건 Optional
 }
