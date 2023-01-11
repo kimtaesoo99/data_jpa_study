e9df0d2a0129fd4eb2ca66eeaf02d9165a14246b
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-11 19:48:23 +0900

    feat: (MemberJpaRepositoryTest) 네임드 쿼리 이용해서 유저 찾기 test

--- src/test/java/com/example/data_jpa_study/repository/MemberJpaRepositoryTest.java
+++ src/test/java/com/example/data_jpa_study/repository/MemberJpaRepositoryTest.java
@@ -72,7 +72,19 @@ class MemberJpaRepositoryTest {
         assertThat(result.get(0).getUsername()).isEqualTo("AAA");
         assertThat(result.get(0).getAge()).isEqualTo(20);
         assertThat(result.size()).isEqualTo(1);
+    }
 
+    @Test
+    public void testNamedQuery(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberJpaRepository.save(m1);
+        memberJpaRepository.save(m2);
+
+        List<Member> result = memberJpaRepository.findByUsername("AAA");
+        Member findMember = result.get(0);
+
+        assertThat(findMember).isEqualTo(m1);
     }
 }
 
