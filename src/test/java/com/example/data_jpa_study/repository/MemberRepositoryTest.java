135bdc4c22849a54bfbf4e0361acefb9e2c3ec67
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-11 22:08:32 +0900

    feat: (MemberRepositoryTest) 벌크 연산 테스트

--- src/test/java/com/example/data_jpa_study/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/data_jpa_study/repository/MemberRepositoryTest.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -192,6 +191,25 @@ class MemberRepositoryTest {
         assertThat(page.getTotalPages()).isEqualTo(2);
         assertThat(page.isFirst()).isTrue();
         assertThat(page.hasNext()).isTrue();
+    }
+
+    @Test
+    public void bulkUpdate(){
+        //given
+        memberRepository.save(new Member("member1",10));
+        memberRepository.save(new Member("member2",19));
+        memberRepository.save(new Member("member3",20));
+        memberRepository.save(new Member("member4",21));
+        memberRepository.save(new Member("member5",40));
+
+        //when
+        int result = memberRepository.bulkAgePlus(20);
+
+        Member member5 = memberRepository.findMemberByUsername("member5");
+        System.out.println("member5 = " + member5);
+
+        //then
+        assertThat(result).isEqualTo(3);
     }
 
 }
