a1c966e9a5fe98daf561d66418ccd566d111941c
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-11 13:59:17 +0900

    test: (MemberJpaRepositoryTest) 기존 Jpa 레포 테스트 추가

---
+++ src/test/java/com/example/data_jpa_study/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.example.data_jpa_study.repository;
+
+
+import com.example.data_jpa_study.entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember(){
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
