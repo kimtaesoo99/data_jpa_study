d69443d42941670c262b23247bf7388b39237ed1
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-11 13:58:18 +0900

    feat: (MemberJpaRepository) 기존 JPA 레포 추가

---
+++ src/main/java/com/example/data_jpa_study/repository/MemberJpaRepository.java
@@ -0,0 +1,23 @@
+package com.example.data_jpa_study.repository;
+
+import com.example.data_jpa_study.entity.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberJpaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Member save(Member member){
+        em.persist(member);
+        return member;
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}
