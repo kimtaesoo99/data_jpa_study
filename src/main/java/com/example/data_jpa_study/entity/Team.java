f08af60a0af02f743c857ef3b7436c8704bab995
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-11 14:58:26 +0900

    feat: (Team) 멤버와 일대다 매핑

---
+++ src/main/java/com/example/data_jpa_study/entity/Team.java
@@ -0,0 +1,28 @@
+package com.example.data_jpa_study.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.LinkedList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","name"})
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new LinkedList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
