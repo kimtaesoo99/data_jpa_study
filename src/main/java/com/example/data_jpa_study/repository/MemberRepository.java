2341dc2e840648ea01b900fb26f525c11a91513f
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-11 20:11:25 +0900

    feat: (MemberRepository) Dto로 바로 조회하는 기능 추가

--- src/main/java/com/example/data_jpa_study/repository/MemberRepository.java
+++ src/main/java/com/example/data_jpa_study/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.example.data_jpa_study.repository;
 
+import com.example.data_jpa_study.dto.MemberDto;
 import com.example.data_jpa_study.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +17,10 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
 
     @Query("select m from Member m where m.username =:username and m.age =:age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new com.example.data_jpa_study.dto.MemberDto(m.id, m.username,t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
