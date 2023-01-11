cb946fa76dfaa09b83c4d7c95bf7611cc67ff8f8
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-11 13:11:37 +0900

    Initial commit

---
+++ src/main/java/com/example/data_jpa_study/DataJpaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.data_jpa_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/data_jpa_study/DataJpaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data_jpa_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
