095e6681b48a7d6ab3d02f8aab37195e5b014998
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-11 15:48:38 +0900

    feat: (MemberRepository) 원하는 이름과 나이 제한보다 높은 사람 조회기능 추가

--- src/main/java/com/example/data_jpa_study/repository/MemberRepository.java
+++ src/main/java/com/example/data_jpa_study/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.example.data_jpa_study.repository;
 import com.example.data_jpa_study.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member,Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username,int age);
 }
