3cd78fe0993ede1caa9a3c9685d723ca84ee35b7
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-13 20:22:26 +0900

    feat: (Item) 상품 엔티티 추가

---
+++ src/main/java/com/example/data_jpa_study/entity/Item.java
@@ -0,0 +1,39 @@
+package com.example.data_jpa_study.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Item implements Persistable<String> {
+
+    @Id
+    private String id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdDate == null;
+    }
+
+    public Item(String id) {
+        this.id = id;
+    }
+}
