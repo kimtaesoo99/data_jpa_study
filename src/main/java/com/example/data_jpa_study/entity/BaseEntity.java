aab84f8b10e46eaf3f16f861ee58fe25e3acb014
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-13 14:00:25 +0900

    feat: (BaseEntity) 등록자 수정자 추가

---
+++ src/main/java/com/example/data_jpa_study/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.example.data_jpa_study.entity;
+
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity extends BaseTimeEntity{
+
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+}
