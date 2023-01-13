78037f0c3fe150b6946e94afc84e8a51e5bb3c1f
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-13 14:01:39 +0900

    feat: (Application) 등록자, 수정자를 처리해주는 AuditorAware 스프링 빈 등록

--- src/main/java/com/example/data_jpa_study/DataJpaStudyApplication.java
+++ src/main/java/com/example/data_jpa_study/DataJpaStudyApplication.java
@@ -2,12 +2,23 @@ package com.example.data_jpa_study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DataJpaStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DataJpaStudyApplication.class, args);
     }
 
+    @Bean
+    public AuditorAware<String> auditorProvider(){
+        return () -> Optional.of(UUID.randomUUID().toString());
+    }
 }
