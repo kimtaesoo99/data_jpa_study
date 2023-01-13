6a681ced8505e5cd86ac772a86b54a93ac0af226
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-13 22:12:41 +0900

    feat: (MemberRepositoryTest) 네이티브 쿼리 테스트 추가

--- src/test/java/com/example/data_jpa_study/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/data_jpa_study/repository/MemberRepositoryTest.java
@@ -319,4 +319,28 @@ class MemberRepositoryTest {
             System.out.println("usernameOnly = " + usernameOnly.getUsername());
         }
     }
+
+    @Test
+    public void nativeQuery(){
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamA);
+        em.persist(member1);
+        em.persist(member2);
+
+        em.flush();
+        em.clear();
+
+        //when
+        Page<MemberProjection> result = memberRepository.findByNativeProjection(PageRequest.of(1, 10));
+        List<MemberProjection> content = result.getContent();
+        for (MemberProjection memberProjection : content) {
+            System.out.println("memberProjection = " + memberProjection.getUsername());
+            System.out.println("memberProjection = " + memberProjection.getTeamName());
+        }
+    }
+
 }
