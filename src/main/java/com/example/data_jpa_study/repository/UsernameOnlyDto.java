c2458a01c23ee89b1d6f7cfb00f27f6ef774e407
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-13 21:10:00 +0900

    feat: (UsernameOnlyDto) 유저이름만 받는 Dto 추가

---
+++ src/main/java/com/example/data_jpa_study/repository/UsernameOnlyDto.java
@@ -0,0 +1,13 @@
+package com.example.data_jpa_study.repository;
+
+public class UsernameOnlyDto {
+    private final String username;
+
+    public UsernameOnlyDto(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
