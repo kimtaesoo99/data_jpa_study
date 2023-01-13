78d85908dddb5dd59acb547973f94265fcd4daaf
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-13 14:43:36 +0900

    feat: (MemberController) 페이징 처리후 조회추가

--- src/main/java/com/example/data_jpa_study/controller/MemberController.java
+++ src/main/java/com/example/data_jpa_study/controller/MemberController.java
@@ -1,9 +1,14 @@
 package com.example.data_jpa_study.controller;
 
 
+import com.example.data_jpa_study.dto.MemberDto;
 import com.example.data_jpa_study.entity.Member;
 import com.example.data_jpa_study.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.engine.jdbc.Size;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,8 +32,16 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable){
+        return memberRepository.findAll(pageable)
+            .map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+    }
+
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++){
+            memberRepository.save(new Member("user"+i,i));
+        }
     }
 }
